ef43ae2fef56f0d877befaa198668eea885aef48
dawit-yit <dawityitbarek18@gmail.com>
2025-04-05 13:26:22 +0300

    second test

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL.includes('sslmode=require') ? { rejectUnauthorized: false } : false
 });
-console.log("The ssl",ssl);
 console.log("down to the ssl configuration")
 
 
